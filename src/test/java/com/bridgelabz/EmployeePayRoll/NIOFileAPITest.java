70af2e9b0cfaf6934e33fdb70031a47b78550313
ppgupta <premprakashnayak69@gmail.com>
2020-10-16 17:33:04 +0530

    file operation demonstrate

---
+++ src/test/java/com/bridgelabz/EmployeePayRoll/NIOFileAPITest.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.EmployeePayRoll;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class NIOFileAPITest {
+	private static String HOME = System.getProperty("user.home");
+	private static String PLAY_WITH_NIO = "temp_play_ground";
+	
+	@Test
+	public void givenPathWhenCheckedThenConfirmUC1() throws IOException {
+		
+		//check if file exists
+		Path path = Paths.get(HOME);
+		assertTrue(Files.exists(path));
+		
+		//Delete file and check if file exist or not
+		Path playPath = Paths.get(HOME+"/"+PLAY_WITH_NIO);
+		if(Files.exists(playPath))
+			FileUtils.deleteFiles(playPath.toFile());
+		assertTrue(Files.notExists(playPath));
+		
+		//create directory
+		Files.createDirectory(playPath);
+		assertTrue(Files.exists(playPath));
+		
+		//create file
+		IntStream.range(1,10).forEach(n->{
+			Path tempFile = Paths.get(playPath+"/temp"+n);
+			assertTrue(Files.notExists(tempFile));
+			try {
+				Files.createFile(tempFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			assertTrue(Files.exists(tempFile));
+		});
+	}
+}
